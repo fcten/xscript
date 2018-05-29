a73eebc6b3d97e40f0abfb6faf1502c3aa42c8af
fcten <huangke5555@163.com>
2018-05-29 15:30:06 +0800

    更新保留字

--- src/tokenizer/tokens.c
+++ src/tokenizer/tokens.c
@@ -17,8 +17,60 @@ class
 
 // 关键字 && 保留字
 const lgx_token_t lgx_reserved_words[LGX_RESERVED_WORDS] = {
-    {TK_AUTO, "auto"},    {TK_FOR, "for"},         {TK_DO, "do"},
-    {TK_WHILE, "while"},  {TK_BREAK, "break"},     {TK_CONTINUE, "continue"},
-    {TK_IF, "if"},        {TK_ELSE, "else"},       {TK_SWITCH, "switch"},
-    {TK_CASE, "case"},    {TK_DEFAULT, "default"}, {TK_FUNCTION, "function"},
-    {TK_RETURN, "return"}};
+    // 变量
+    {TK_AUTO, "auto"},
+    {TK_INT, "int"},
+    {TK_FLOAT, "float"},
+    {TK_BOOL, "bool"},
+    {TK_STR, "string"},
+    {TK_ARRAY, "array"},
+    {TK_OBJECT, "object"},
+    // 特殊值
+    {TK_TRUE, "true"},
+    {TK_FALSE, "false"},
+    {TK_NULL, "null"},
+    // 函数 & 方法
+    {TK_FUNCTION, "function"},
+    {TK_RETURN, "return"},
+    // 类
+    {TK_THIS, "this"},
+    {TK_NEW, "new"},
+    {TK_DELETE, "delete"},
+    {TK_CLASS, "class"},
+    {TK_EXTENDS, "extends"},
+    {TK_PUBLIC, "public"},
+    {TK_PRIVATE, "private"},
+    {TK_PROTECTED, "protected"},
+    {TK_FINAL, "final"},
+    {TK_INTERFACE, "interface"},
+    {TK_IMPLEMENTS, "implements"},
+    // 流程控制
+    {TK_IF, "if"},
+    {TK_ELSE, "else"},
+    {TK_SWITCH, "switch"},
+    {TK_CASE, "case"},
+    {TK_DEFAULT, "default"},
+    // 循环
+    {TK_FOR, "for"},
+    {TK_DO, "do"},
+    {TK_WHILE, "while"},
+    {TK_FOREACH, "foreach"},
+    // 控制转移
+    {TK_BREAK, "break"},
+    {TK_CONTINUE, "continue"},
+    // 异常
+    {TK_TRY, "try"},
+    {TK_CATCH, "catch"},
+    {TK_FINALLY, "finally"},
+    {TK_THROW, "throw"},
+    // 模块
+    {TK_IMPORT, "import"},
+    {TK_EXPORT, "export"},
+
+    {TK_CONST, "const"},
+    {TK_STATIC, "static"},
+    
+    {TK_NAMESPACE, "namespace"},
+    
+    {TK_USE, "use"},
+    {TK_AS, "as"}};
--- src/tokenizer/tokens.h
+++ src/tokenizer/tokens.h
@@ -2,34 +2,79 @@
 #define LGX_TOKENS_H
 
 enum {
-    TK_ID = 256,
-    TK_NUMBER,  // 257
-    TK_STRING,  // 258
-    TK_EOF,     // 259
-    TK_EOL,     // 260
-    TK_SPACE,   // 261
-    TK_EQ,      // 262
-    TK_NE,      // 263
-    TK_GE,      // 264
-    TK_LE,      // 265
-    TK_AND,     // 266
-    TK_OR,      // 267
-    TK_SHL,     // 268
-    TK_SHR,     // 269
-    TK_COMMENT, // 270
-    TK_AUTO,    // 271
-    TK_FOR,     // 272
-    TK_DO,      // 273
-    TK_WHILE,   // 274
-    TK_BREAK,   // 275
-    TK_CONTINUE,// 276
-    TK_IF,      // 277
-    TK_ELSE,    // 278
-    TK_SWITCH,  // 279
-    TK_CASE,    // 280
-    TK_DEFAULT, // 281
-    TK_FUNCTION,// 282
-    TK_RETURN,  // 283
+    TK_ID = 256,// 256 标识符
+    TK_NUMBER,  // 257 立即数
+    TK_STRING,  // 258 字符串常量
+    TK_EOF,     // 259 文件结束标志
+    TK_EOL,     // 260 行末标志
+    TK_SPACE,   // 261 空格 & 制表符
+    TK_EQ,      // 262 ==
+    TK_NE,      // 263 !=
+    TK_GE,      // 264 >=
+    TK_LE,      // 265 <=
+    TK_AND,     // 266 &&
+    TK_OR,      // 267 ||
+    TK_SHL,     // 268 <<
+    TK_SHR,     // 269 >>
+    TK_COMMENT, // 270 代码注释
+    // 以下为关键字 & 保留字
+    TK_AUTO,
+    TK_INT,
+    TK_FLOAT,
+    TK_BOOL,
+    TK_STR,
+    TK_ARRAY,
+    TK_OBJECT,
+
+    TK_TRUE,
+    TK_FALSE,
+    TK_NULL,
+
+    TK_FUNCTION,
+    TK_RETURN,
+
+    TK_THIS,
+    TK_NEW,
+    TK_DELETE,
+    TK_CLASS,
+    TK_EXTENDS,
+    TK_PUBLIC,
+    TK_PRIVATE,
+    TK_PROTECTED,
+    TK_FINAL,
+    TK_INTERFACE,
+    TK_IMPLEMENTS,
+
+    TK_IF,
+    TK_ELSE,
+    TK_SWITCH,
+    TK_CASE,
+    TK_DEFAULT,
+
+    TK_FOR,
+    TK_DO,
+    TK_WHILE,
+    TK_FOREACH,
+
+    TK_BREAK,
+    TK_CONTINUE,
+
+    TK_TRY,
+    TK_CATCH,
+    TK_FINALLY,
+    TK_THROW,
+
+    TK_IMPORT,
+    TK_EXPORT,
+
+    TK_CONST,
+    TK_STATIC,
+
+    TK_NAMESPACE,
+    
+    TK_USE,
+    TK_AS,
+
     TK_UNKNOWN = 1024
 };
 
@@ -38,6 +83,6 @@ typedef struct {
     char* s;
 } lgx_token_t;
 
-#define LGX_RESERVED_WORDS  13
+#define LGX_RESERVED_WORDS  (TK_AS - TK_AUTO + 1) 
 
 #endif // LGX_TOKENS_H
