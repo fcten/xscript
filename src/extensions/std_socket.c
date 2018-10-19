6631429df2ed20b5767358604f0ef5365e8b1869
fcten <huangke5555@163.com>
2018-10-19 21:27:35 +0800

    完善网络标准库

--- src/extensions/std_socket.c
+++ src/extensions/std_socket.c
@@ -2,7 +2,24 @@
 #include "../common/fun.h"
 #include "std_coroutine.h"
 
-int std_socket_accept(void *p) {
+int std_socket_server_create(void *p) {
+    lgx_vm_t *vm = p;
+
+    unsigned base = vm->regs[0].v.fun->stack_size;
+    long long port = vm->regs[base+4].v.l;
+
+    // 创建监听端口并加入事件循环
+
+    return 0;
+}
+
+int std_socket_server_send(void *p) {
+    lgx_vm_t *vm = p;
+
+    return 0;
+}
+
+int std_socket_server_close(void *p) {
     lgx_vm_t *vm = p;
 
     return 0;
@@ -12,10 +29,12 @@ int std_socket_load_symbols(lgx_hash_t *hash) {
     lgx_val_t symbol;
 
     symbol.type = T_FUNCTION;
-    symbol.v.fun = lgx_fun_new(0);
-    symbol.v.fun->buildin = std_socket_accept;
+    symbol.v.fun = lgx_fun_new(1);
+    symbol.v.fun->buildin = std_socket_server_create;
 
-    if (lgx_ext_add_symbol(hash, "accept", &symbol)) {
+    symbol.v.fun->args[0].type = T_LONG;
+
+    if (lgx_ext_add_symbol(hash, "server_create", &symbol)) {
         return 1;
     }
 
