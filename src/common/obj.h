190dde77375ea18becad9f24de7e90ef902f996f
fcten <huangke5555@163.com>
2018-11-01 00:49:00 +0800

    准备添加对象支持

---
+++ src/common/obj.h
@@ -0,0 +1,6 @@
+#ifndef LGX_OBJ_H
+#define	LGX_OBJ_H
+
+#include "val.h"
+
+#endif // LGX_OBJ_H
--- src/common/typedef.h
+++ src/common/typedef.h
@@ -3,17 +3,26 @@
 
 #include "list.h"
 
-typedef struct {
+typedef struct lgx_val_s  lgx_val_t;
+typedef struct lgx_str_s  lgx_str_t;
+typedef struct lgx_hash_s lgx_hash_t;
+typedef struct lgx_obj_s  lgx_obj_t;
+typedef struct lgx_res_s  lgx_res_t;
+typedef struct lgx_ref_s  lgx_ref_t;
+typedef struct lgx_fun_s  lgx_fun_t;
+typedef struct lgx_gc_s   lgx_gc_t;
+
+struct lgx_val_s {
     union {
         long long         l; // 64 位有符号整数
         double            d; // 64 位有符号浮点数
-        struct lgx_str_s  *str;
-        struct lgx_hash_s *arr;
-        struct lgx_obj_s  *obj;
-        struct lgx_res_s  *res;
-        struct lgx_ref_s  *ref;
-        struct lgx_fun_s  *fun;
-        struct lgx_gc_s   *gc; // 方便访问任意高级类型的 gc 字段，gc 字段必须在高级类型结构体的头部
+        lgx_str_t  *str;
+        lgx_hash_t *arr;
+        lgx_obj_t  *obj;
+        lgx_res_t  *res;
+        lgx_ref_t  *ref;
+        lgx_fun_t  *fun;
+        lgx_gc_t   *gc; // 方便访问任意高级类型的 gc 字段，gc 字段必须在高级类型结构体的头部
     } v;
     unsigned char type;
     // 这个 union 结构有 7 个字节可用
@@ -31,20 +40,14 @@ typedef struct {
         } c;
         // 调试信息，运行时使用
     } u;
-} lgx_val_t;
+};
 
 typedef struct {
     lgx_list_t head;
     lgx_val_t *v;
 } lgx_val_list_t;
 
-typedef struct lgx_obj_s {
-} lgx_obj_t;
-
-typedef struct lgx_res_s {
-} lgx_res_t;
-
-typedef struct lgx_gc_s {
+struct lgx_gc_s {
     // 双向链表
     lgx_list_t head;
     // GC 对象的内存占用，字节
@@ -55,32 +58,9 @@ typedef struct lgx_gc_s {
     unsigned char type;
     // GC 标记
     unsigned char color;
-} lgx_gc_t;
+};
 
-typedef struct lgx_fun_s {
-    // GC 信息
-    lgx_gc_t gc;
-    // 需求的堆栈空间
-    unsigned stack_size;
-    // 入口地址
-    unsigned addr;
-    // 内建函数指针
-    int (*buildin)(void *vm);
-    // 返回值
-    lgx_val_t ret;
-    // 参数列表
-    unsigned args_num;
-    lgx_val_t args[];
-} lgx_fun_t;
-
-typedef struct lgx_ref_s {
-    // GC 信息
-    lgx_gc_t gc;
-    // 所引用的变量
-    lgx_val_t v;
-} lgx_ref_t;
-
-typedef struct lgx_str_s {
+struct lgx_str_s {
     // GC 信息
     lgx_gc_t gc;
 
@@ -92,7 +72,32 @@ typedef struct lgx_str_s {
     unsigned is_ref;
     // 指针
     char *buffer;
-} lgx_str_t;
+};
+
+struct lgx_fun_s {
+    // GC 信息
+    lgx_gc_t gc;
+    // 函数名称
+    lgx_str_t name;
+    // 需求的堆栈空间
+    unsigned stack_size;
+    // 入口地址
+    unsigned addr;
+    // 内建函数指针
+    int (*buildin)(void *vm);
+    // 返回值
+    lgx_val_t ret;
+    // 参数列表
+    unsigned args_num;
+    lgx_val_t args[];
+};
+
+struct lgx_ref_s {
+    // GC 信息
+    lgx_gc_t gc;
+    // 所引用的变量
+    lgx_val_t v;
+};
 
 typedef struct lgx_hash_node_s {
     struct lgx_hash_node_s *next;
@@ -104,7 +109,7 @@ typedef struct lgx_hash_node_s {
 // TODO lgx_hash_t 太大了 size=8 时需要多达 416 字节。
 // lgx_hash_t 32 字节
 // lgx_hash_node_t 48 字节
-typedef struct lgx_hash_s {
+struct lgx_hash_s {
     // GC 信息
     lgx_gc_t gc;
 
@@ -126,6 +131,30 @@ typedef struct lgx_hash_s {
 
     // 存储数据的结构
     lgx_hash_node_t* table;
-} lgx_hash_t;
+};
+
+struct lgx_obj_s {
+    // GC 信息
+    lgx_gc_t gc;
+    // 类名称
+    lgx_str_t name;
+    // 是否为内置（否则为用户定义）
+    char is_buildin;
+    // 是否为接口（否则为类）
+    char is_interface;
+    // 继承的父类
+    lgx_obj_t *parent;
+    // 属性 is_static public/protected/private
+    lgx_hash_t properties;
+    // 方法
+    lgx_hash_t methods;
+    // 常量
+    lgx_hash_t constants;
+};
+
+struct lgx_res_s {
+    // GC 信息
+    lgx_gc_t gc;
+};
 
 #endif // LGX_TYPEDEF_H
