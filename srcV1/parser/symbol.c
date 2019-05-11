2fa71ae9b4b2139a5580ea9ae84c9b4fe1333a0e
fcten <huangke5555@163.com>
2019-05-12 01:09:09 +0800

    完善了符号表模块

--- srcV1/parser/symbol.c
+++ srcV1/parser/symbol.c
@@ -1,31 +1,185 @@
 #include "symbol.h"
 
-int symbol_add(lgx_ht_t *symbols, lgx_symbol_type_t type, lgx_str_t s_name, lgx_value_t s_type) {
+// 追加一条错误信息
+static void symbol_error(lgx_ast_t* ast, lgx_ast_node_t* node, const char *fmt, ...) {
+    va_list   args;
+
+    lgx_ast_error_list_t* err = xcalloc(1, sizeof(lgx_ast_error_list_t));
+    if (!err) {
+        return;
+    }
+
+    lgx_list_init(&err->head);
+    err->err_no = 1;
+
+    if (lgx_str_init(&err->err_msg, 256)) {
+        xfree(err);
+        return;
+    }
+
+    if (ast->lex.source.path) {
+        err->err_msg.length = snprintf(err->err_msg.buffer, err->err_msg.size,
+            "[ERROR] [%s:%d:%d] ", ast->lex.source.path, node->line, node->row);
+    } else {
+        err->err_msg.length = snprintf(err->err_msg.buffer, err->err_msg.size,
+            "[ERROR] ");
+    }
+
+    va_start(args, fmt);
+    err->err_msg.length += vsnprintf(err->err_msg.buffer + err->err_msg.length,
+        256 - err->err_msg.length, fmt, args);
+    va_end(args);
+    
+    lgx_list_add_tail(&err->head, &ast->errors);
+}
+
+static int symbol_add(lgx_ast_t* ast, lgx_ast_node_t* node, lgx_ht_t *symbols, lgx_symbol_type_t type) {
+    assert(node->type == IDENTIFIER_TOKEN);
+
+    lgx_str_t name;
+    name.buffer = ast->lex.source.content + node->offset;
+    name.length = node->length;
+
+    switch (type) {
+    case S_VARIABLE:
+        symbol_error(ast, node, "[variable] %.*s\n", name.length, name.buffer);
+        break;
+    case S_CONSTANT:
+        symbol_error(ast, node, "[constant] %.*s\n", name.length, name.buffer);
+        break;
+    default:
+        break;
+    }
+
+    return 1;
+}
+
+static int symbol_add_variable(lgx_ast_t* ast, lgx_ast_node_t* node) {
+    lgx_ast_node_t* id = node->child[0];
+
+    switch (node->parent->type) {
+        case BLOCK_STATEMENT: { // 在块作用域内定义变量
+            // 添加变量到该块作用域中
+            if (symbol_add(ast, id, node->parent->u.symbols, S_VARIABLE)) {
+                return 1;
+            }
+            break;
+        }
+        case FOR_STATEMENT: { // 在 for 循环条件中定义变量
+            // TODO 必须是第一个循环条件
+            if (0) {
+                symbol_error(ast, id, "[invalid variable declaration] %.*s\n", id->length, ast->lex.source.content + id->offset);
+                return 1;
+            }
+
+            // TODO 添加变量到该 FOR 循环的块作用域中
+            break;
+        }
+        case FUNCTION_DECL_PARAMETER: { // 在函数参数列表中定义变量
+            // TODO 添加变量到该函数的块作用域中
+            break;
+        }
+        default:
+            symbol_error(ast, id, "[invalid variable declaration] %.*s\n", id->length, ast->lex.source.content + id->offset);
+            return 1;
+    }
+
     return 0;
 }
 
-int symbol_parse(lgx_ast_t* ast, lgx_ast_node_t* node) {
+static int symbol_add_constant(lgx_ast_t* ast, lgx_ast_node_t* node) {
+    lgx_ast_node_t* id = node->child[0];
+
+    switch (node->parent->type) {
+        case BLOCK_STATEMENT: { // 在块作用域内定义常量
+            // 添加常量到该块作用域中
+            if (symbol_add(ast, id, node->parent->u.symbols, S_CONSTANT)) {
+                return 1;
+            }
+            break;
+        }
+        default:
+            symbol_error(ast, id, "[invalid constant declaration] %.*s\n", id->length, ast->lex.source.content + id->offset);
+            return 1;
+    }
+
+    return 0;
+}
+
+static int symbol_add_function(lgx_ast_t* ast, lgx_ast_node_t* node) {
+    lgx_ast_node_t* id = node->child[0];
+
+    switch (node->parent->type) {
+        case BLOCK_STATEMENT: { // 在块作用域内定义函数
+            // 添加函数到该块作用域中
+            if (symbol_add(ast, id, node->parent->u.symbols, S_CONSTANT)) {
+                return 1;
+            }
+            break;
+        }
+        default:
+            symbol_error(ast, id, "[invalid function declaration] %.*s\n", id->length, ast->lex.source.content + id->offset);
+            return 1;
+    }
+
+    return 0;
+}
+
+// 检查标识符是否被定义
+static int symbol_check_identifier(lgx_ast_t* ast, lgx_ast_node_t* node) {
+    return 0;
+}
+
+static int symbol_generate(lgx_ast_t* ast, lgx_ast_node_t* node) {
+    int i, ret = 0;
+
     switch (node->type) {
         case PACKAGE_DECLARATION:
+            break;
         case IMPORT_DECLARATION:
+            break;
         case EXPORT_DECLARATION:
-        case VARIABLE_DECLARATION: {
-            if (node->parent->type == BLOCK_STATEMENT) {
-                //symbol_add(node->parent->u.symbols, S_VARIABLE,
-                //    ast->lex.source.content + node->offset, node->length);
-                lgx_ast_node_t* id = node->child[0];
-                printf("[variable] %.*s\n", id->length, ast->lex.source.content + id->offset);
+            break;
+        case VARIABLE_DECLARATION:
+            if (symbol_add_variable(ast, node)) {
+                ret = 1;
             }
-        }
+            break;
         case CONSTANT_DECLARATION:
+            if (symbol_add_constant(ast, node)) {
+                ret = 1;
+            }
+            break;
         case FUNCTION_DECLARATION:
+            if (symbol_add_function(ast, node)) {
+                ret = 1;
+            }
+            break;
         case TYPE_DECLARATION:
+            break;
         default: break;
     }
 
-    int i, ret = 0;
     for (i = 0; i < node->children; ++i) {
-        if (symbol_parse(ast, node->child[i])) {
+        if (symbol_generate(ast, node->child[i])) {
+            ret = 1;
+        }
+    }
+    
+    return ret;
+}
+
+static int symbol_check(lgx_ast_t* ast, lgx_ast_node_t* node) {
+    int i, ret = 0;
+
+    if (node->type == IDENTIFIER_TOKEN) {
+        if (symbol_check_identifier(ast, node)) {
+            ret = 1;
+        }
+    }
+
+    for (i = 0; i < node->children; ++i) {
+        if (symbol_check(ast, node->child[i])) {
             ret = 1;
         }
     }
@@ -35,5 +189,9 @@ int symbol_parse(lgx_ast_t* ast, lgx_ast_node_t* node) {
 
 // 遍历语法树生成符号信息
 int lgx_symbol_init(lgx_ast_t* ast) {
-    return symbol_parse(ast, ast->root);
+    if (symbol_generate(ast, ast->root)) {
+        return 1;
+    }
+
+    return symbol_check(ast, ast->root);
 }
